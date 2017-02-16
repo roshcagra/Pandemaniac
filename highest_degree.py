5e92babc9ce8ff0df4f6da59e131762022881fab
roshcagra <roshan.agrawal95@gmail.com>
2017-02-15 18:20:30 -0800

    rewrite highest_degree

--- highest_degree.py
+++ highest_degree.py
@@ -1,17 +1,7 @@
-from utils import read_graph
-from utils import write_nodes
-import operator
-import sys
+def highest_degree(G):
+    G_degrees = dict(map(lambda (k, v): (k, len(v)), G.iteritems()))
 
-filename = sys.argv[1]
-n = int(sys.argv[2])
+    G_sorted = sorted(G_degrees.items(), key=operator.itemgetter(1), reverse=True)
 
-G = read_graph(filename)
-
-G_degrees = dict(map(lambda (k, v): (k, len(v)), G.iteritems()))
-
-G_sorted = sorted(G_degrees.items(), key=operator.itemgetter(1), reverse=True)
-
-top_n = [node for (node, _) in G_sorted[0:n]]
-
-write_nodes(top_n)
+    top_n = [node for (node, _) in G_sorted[0:n]]
+    return top_n;
