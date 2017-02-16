829a9ba2f975c4936e32c5e239546d65d6bdb15d
roshcagra <roshan.agrawal95@gmail.com>
2017-02-15 18:20:46 -0800

    n

--- highest_degree.py
+++ highest_degree.py
@@ -1,4 +1,4 @@
-def highest_degree(G):
+def highest_degree(G, n):
     G_degrees = dict(map(lambda (k, v): (k, len(v)), G.iteritems()))
 
     G_sorted = sorted(G_degrees.items(), key=operator.itemgetter(1), reverse=True)
