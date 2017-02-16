95348b1e22866d6072ddaa82e63955bbc6f2aee0
roshcagra <roshan.agrawal95@gmail.com>
2017-02-15 18:34:40 -0800

    Get highest degree in cluster

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -3,6 +3,7 @@ from utils import write_nodes
 from utils import adjacency_list_to_incidence_matrix
 import operator
 import sys
+import numpy as np
 
 from sklearn.cluster import spectral_clustering
 
@@ -10,6 +11,20 @@ filename = sys.argv[1]
 n = int(sys.argv[2])
 
 G = read_graph(filename)
+
+G_degrees = dict(map(lambda (k, v): (k, len(v)), G.iteritems()))
+
 i_matrix = adjacency_list_to_incidence_matrix(G)
 
 clusters = spectral_clustering(i_matrix)
+
+for i in range(8):
+    curr_cluster = np.where(clusters==i)
+    best_node = -1
+    best_degree = -1
+    for node in np.nditer(curr_cluster):
+        node_degree = G_degrees[str(node)]
+        if node_degree > best_degree:
+            best_node = node
+            best_degree = node_degree
+    print "Cluster", i, best_node
